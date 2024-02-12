dc43643708b9a1ffc21d6cd403821fd79d83ca94
Donovan Burns <24DMB02@stu.cpsk12.org>
2024-02-12 15:42:28 -0600

    commit

--- Gauntlet/BoundsManager.h
+++ Gauntlet/BoundsManager.h
@@ -1,5 +1,16 @@
 #pragma once
+#include "SharedTypes.h"
+
 class BoundsManager
 {
+public:
+	BoundsManager();
+	~BoundsManager();
+
+	bool isColliding();
+	void setHitbox();
+
+private:
+	TileRegion regions[];
 };
 
