0460a7a78aa9389ab8ae1c44e2d9ee9b26fdb605
KingZhara <waroperationplannedresponse@protonmail.com>
2024-02-12 15:37:36 -0600

    create base resource manager

--- Gauntlet/ResourceManager.h
+++ Gauntlet/ResourceManager.h
@@ -1,5 +1,27 @@
 #pragma once
+
+#include "SharedTypes.h"
+
+
 class ResourceManager
 {
+public:
+	ResourceManager();
+	~ResourceManager();
+
+
+private:
+	struct ManagedResource
+	{
+		const UsableResource resource;
+		const ResourceIdentifier identifier;
+
+		ManagedResource(int count, 
+			UsableResource initResource, ResourceIdentifier initID)
+			: resource(initResource), identifier(initID) {}
+	};
+
+	ManagedResource* textures;
+	ManagedResource* sounds;
 };
 
