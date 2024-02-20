4d7448283459e7f262d7426fd1dd50a730e10ecb
Donovan Burns <24DMB02@stu.cpsk12.org>
2024-02-20 15:27:35 -0600

    TileEntity

--- Gauntlet/TileEntity.h
+++ Gauntlet/TileEntity.h
@@ -5,5 +5,30 @@
 
 class TileEntity : public Entity
 {
-};
+public:
+	enum TileType
+	{
+		DEMON_S,
+		GHOST_S,
+		GRUNT_S,
+		LOBBER_S,
+		SORCERER_S,
+		THIEF_S,
+		KEY,
+		BOMB_POTION,
+		INVISIBILITY_POTION,
+		EXTRA_POWER_POTION,
+		MEAT_1,
+		MEAT_2,
+		BOOZE,
+		POISON
+	};
+	
+	TileEntity(TileType type);
+	~TileEntity();
 
+	void isPoison(TileType type);
+	void method(TileType type);
+
+private:
+};
