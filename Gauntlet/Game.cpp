5495e7be82da63e9e678eafe7bd37667d0ad90b0
KingZhara <waroperationplannedresponse@protonmail.com>
2024-02-20 14:54:55 -0600

    Create game base

--- Gauntlet/Game.cpp
+++ Gauntlet/Game.cpp
@@ -1,1 +1,32 @@
 #include "Game.h"
+
+
+Game::Game()
+{
+	//resourceManager.l
+	timer.restart();
+}
+
+Game::~Game()
+{
+}
+
+void Game::start()
+{
+}
+
+void Game::tick()
+{
+
+
+	deltatime = timer.getElapsedTime().asMilliseconds();
+	timer.restart();
+}
+
+void Game::entityTick()
+{
+}
+
+void Game::draw()
+{
+}
--- Gauntlet/Game.h
+++ Gauntlet/Game.h
@@ -3,15 +3,27 @@
 #include "ResourceManager.h"
 #include "BoundsManager.h"
 #include "Floor.h"
+#include "Entity.h"
+#include <SFML/Graphics.hpp>
 
 class Game
 {
 public:
+	Game();
+	~Game();
+
+	void start();
+
 private:
 	double deltatime = 0;
 	ResourceManager resourceManager;
 	BoundsManager boundsManager;
 	Floor floor;
+	Entity* entities;
+	sf::Clock timer;
 
+	void tick();
+	void entityTick();
+	void draw();
 };
 
